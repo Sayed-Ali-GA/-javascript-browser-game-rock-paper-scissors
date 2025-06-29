0c152a232664dc4c366c967a76694fa011cc1837
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-06-29 10:46:31 +0300

    Cache button and create state variables

---
+++ js/app.js
@@ -0,0 +1,47 @@
+/*-------------------------------- Constants --------------------------------*/
+
+ const choice = ['rock' , 'paper' , 'scissors']
+
+
+
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+let msg
+let playerChoice
+let computerChoice
+
+
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+ const rockBtnEl = document.querySelector('#rock')
+ console.log(rockBtnEl)
+
+const paperBtnEl = document.querySelector('#paper')
+console.log(paperBtnEl)
+
+const scissoreBtnEl = document.querySelector('#scissors')
+console.log(scissoreBtnEl)
+
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
